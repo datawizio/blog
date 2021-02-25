3aa01bb6929ed9fb22758e86269361f0f072b1f9
bockardo <bockardo@gmail.com>
2021-02-25 09:15:50 +0200

    config admin page

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet
+from .models import Post, Comment
 
 # Register your models here.
+
+
+class CommentAdminModelInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdminModel(admin.ModelAdmin):
+    inlines = [CommentAdminModelInline]
+    pass
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).prefetch_related("comments")
--- article/models.py
+++ article/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
