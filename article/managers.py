83fb5adc146b843061c08a6f48c7a08de3255276
bockardo <bockardo@gmail.com>
2021-02-26 10:46:34 +0200

    rename files

--- article/managers.py
+++ article/managers.py
@@ -1,9 +1,8 @@
 from django.db.models import Manager
-from . import queryset as article_queryset
+from . import querysets as article_queryset
 
 
 class PostManager(Manager):
-
     def get_queryset(self) -> article_queryset.PostQuerySet:
         return article_queryset.PostQuerySet(self.model, using=self._db)
 
--- article/models.py
+++ article/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from .manager import PostManager
+from .managers import PostManager
 
 User = get_user_model()
 
