920c04be4009fd1e74d66a5708e99e9915729cc7
Ivan Osypov <iosypov.dw@gmail.com>
2021-03-01 18:01:23 +0200

    fix static

--- blog/settings.py
+++ blog/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
