ca9048eafa4b30c86ee766d83178e824b27ea1b1
bockardo <bockardo@gmail.com>
2021-03-02 20:29:21 +0200

    setup heroku files

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,10 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
     "django_extensions",
-
     "account",
     "article",
     "ckeditor",
@@ -126,9 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
@@ -307,3 +309,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "core.pagination.CustomPageNumberPagination",
     "PAGE_SIZE": 100,
 }
+
+django_heroku.settings(locals())
